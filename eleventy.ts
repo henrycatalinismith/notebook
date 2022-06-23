b3ab0817c77a8321b712ec26b8bad9e995d23770
Henry Catalini Smith <henry@catalinismith.com>
2022-06-23 21:45:54 +0200

    just get it building and deploying

--- eleventy.ts
+++ eleventy.ts
@@ -1,28 +1,5 @@
-import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight"
 import { rehypePlugin } from "@henrycatalinismith/11tyhype"
-import { sassPlugin } from "@henrycatalinismith/11tysass"
-import { reactPlugin } from "@henrycatalinismith/11tysnap"
-import { EleventyCollection, EleventyLayout } from "@henrycatalinismith/11tytype"
 import rehypeMinifyWhitespace from "rehype-minify-whitespace"
-import rehypeUrls from "rehype-urls"
-
-declare global {
-  interface Home {
-    title: string
-    description: string
-  }
-
-  interface Note {
-    name: string
-    description: string
-  }
-
-  type Collections = {
-    notes: EleventyCollection<Note>
-  }
-
-  type Layout<Template> = EleventyLayout<Template, Collections>
-}
 
 module.exports = function(eleventyConfig) {
   console.log("notebook")
@@ -34,50 +11,13 @@ module.exports = function(eleventyConfig) {
     }
   )
 
-  eleventyConfig.addFilter(
-    "iso8601",
-    function(d) {
-      const date = new Date(d)
-      return [
-        date.getFullYear(),
-        "-",
-        (date.getMonth() + 1).toString().padStart(2, "0"),
-        "-",
-        date.getDate(),
-      ].join("")
-    }
-  )
-
   eleventyConfig.addPassthroughCopy("notebook.svg")
-  eleventyConfig.addPlugin(syntaxHighlight)
 
   const siteUrl = process.env.CI ? "https://hen.cat/notebook/" : ""
-
-  eleventyConfig.addPlugin(sassPlugin, {
-    files: [{
-      file: "style.scss",
-      outFile: "style.[hash].css",
-      outputStyle: "compressed",
-    }],
-    plugins: [
-      // css => postcss([autoprefixer]).process(css).css,
-      // css => { throw new Error("lol") },
-    ],
-    verbose: true,
-  })
-
-  eleventyConfig.addPlugin(reactPlugin, {
-    verbose: true,
-  })
 
   eleventyConfig.addPlugin(rehypePlugin, {
     plugins: [
       [rehypeMinifyWhitespace],
-      [rehypeUrls, url => {
-        if (url.href.startsWith("/")) {
-          return `${siteUrl}${url.href}`
-        }
-      }],
     ]
   })
 
--- notes/notes.11tydata.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  layout: "note",
-  eleventyComputed: {
-    permalink: data => {
-      return `/${data.page.fileSlug}/index.html`
-    },
-  },
-}
-
